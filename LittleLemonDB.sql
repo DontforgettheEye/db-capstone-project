2f09e546f351133517e96e22e9b743b97e2424da
DontforgettheEye <greigroulston@gmail.com>
2024-08-26 13:20:08 +1200

    added dummy data to database and completed the tasks in module 2, screengrabs included of results

--- LittleLemonDB.sql
+++ LittleLemonDB.sql
@@ -23,12 +23,12 @@ DROP TABLE IF EXISTS `address`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `address` (
-  `AddressID` int NOT NULL,
+  `AddressID` int NOT NULL AUTO_INCREMENT,
   `Street` varchar(255) NOT NULL,
   `AreaCode` int NOT NULL,
   `Suburb` varchar(100) NOT NULL,
   PRIMARY KEY (`AddressID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -37,6 +37,7 @@ CREATE TABLE `address` (
 
 LOCK TABLES `address` WRITE;
 /*!40000 ALTER TABLE `address` DISABLE KEYS */;
+INSERT INTO `address` VALUES (1,'123 Road Street',6035,'Ngaio'),(2,'47 Huntleigh road',6001,'Karori'),(3,'56 karori road',6005,'Newtown');
 /*!40000 ALTER TABLE `address` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -87,7 +88,7 @@ CREATE TABLE `customers` (
   PRIMARY KEY (`CustomerID`),
   KEY `address_id_fk_idx` (`AddressID`),
   CONSTRAINT `address_id_fk` FOREIGN KEY (`AddressID`) REFERENCES `address` (`AddressID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -96,6 +97,7 @@ CREATE TABLE `customers` (
 
 LOCK TABLES `customers` WRITE;
 /*!40000 ALTER TABLE `customers` DISABLE KEYS */;
+INSERT INTO `customers` VALUES (1,'John Johnstone','47555579','John@HungPhone.com',1),(2,'Jenny Jacobson','95734567','JennyJ@gmail.com',2),(3,'Qunicy Jackson','55598734','QJ@gmail.com',3);
 /*!40000 ALTER TABLE `customers` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -111,7 +113,7 @@ CREATE TABLE `menu` (
   `Cuisine` varchar(100) NOT NULL,
   `MenuName` varchar(100) NOT NULL,
   PRIMARY KEY (`MenuID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -120,6 +122,7 @@ CREATE TABLE `menu` (
 
 LOCK TABLES `menu` WRITE;
 /*!40000 ALTER TABLE `menu` DISABLE KEYS */;
+INSERT INTO `menu` VALUES (1,'Japanese','Japan'),(2,'Turkish','Turkey in your tummy'),(3,'Itallian','A taste of Italy');
 /*!40000 ALTER TABLE `menu` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -136,10 +139,11 @@ CREATE TABLE `menuitems` (
   `StarterName` varchar(255) NOT NULL,
   `DesertName` varchar(255) NOT NULL,
   `MenuID` int NOT NULL,
+  `Drink` varchar(150) NOT NULL,
   PRIMARY KEY (`MenuItemsID`),
   KEY `menu_id_fk_idx` (`MenuID`),
   CONSTRAINT `menu_id_fk` FOREIGN KEY (`MenuID`) REFERENCES `menu` (`MenuID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -148,6 +152,7 @@ CREATE TABLE `menuitems` (
 
 LOCK TABLES `menuitems` WRITE;
 /*!40000 ALTER TABLE `menuitems` DISABLE KEYS */;
+INSERT INTO `menuitems` VALUES (4,'Karage Chicken','Sushi','ice cream',1,'miso soup'),(5,'Kebab','falefal','turkish delight',2,'apple tea'),(6,'Pizza','olives','gelato',3,'wine');
 /*!40000 ALTER TABLE `menuitems` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -159,7 +164,7 @@ DROP TABLE IF EXISTS `orderdeliverystatus`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `orderdeliverystatus` (
-  `DeliveryID` int NOT NULL,
+  `DeliveryID` int NOT NULL AUTO_INCREMENT,
   `DeliveryDate` date NOT NULL,
   `Status` varchar(100) NOT NULL,
   `OrderID` int NOT NULL,
@@ -189,20 +194,20 @@ DROP TABLE IF EXISTS `orders`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `orders` (
-  `OrderID` int NOT NULL,
+  `OrderID` int NOT NULL AUTO_INCREMENT,
   `Quantity` int NOT NULL,
   `TotalCost` decimal(10,0) NOT NULL,
   `CustomerID` int NOT NULL,
-  `MenuID` int NOT NULL,
+  `MenuItemID` int NOT NULL,
   `StaffID` int NOT NULL,
   PRIMARY KEY (`OrderID`),
-  KEY `menu_id_fk_idx` (`MenuID`),
   KEY `staff_id_fk_idx` (`StaffID`),
   KEY `customer_id_fk_idx` (`CustomerID`),
+  KEY `o_menu_item_id_fk_idx` (`MenuItemID`),
   CONSTRAINT `o_customer_id_fk` FOREIGN KEY (`CustomerID`) REFERENCES `customers` (`CustomerID`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `o_menu_id_fk` FOREIGN KEY (`MenuID`) REFERENCES `menu` (`MenuID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `o_menu_item_id_fk` FOREIGN KEY (`MenuItemID`) REFERENCES `menuitems` (`MenuItemsID`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `o_staff_id_fk` FOREIGN KEY (`StaffID`) REFERENCES `staff` (`StaffID`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -211,8 +216,23 @@ CREATE TABLE `orders` (
 
 LOCK TABLES `orders` WRITE;
 /*!40000 ALTER TABLE `orders` DISABLE KEYS */;
+INSERT INTO `orders` VALUES (9,2,22,3,5,1),(10,1,4,1,6,2),(11,5,52,3,6,3),(12,2,22,2,5,2),(13,1,101,1,4,2),(14,5,202,2,4,2),(15,10,202,3,5,1),(16,10,301,1,6,2),(17,50,152,3,6,3),(18,25,22,2,5,2);
 /*!40000 ALTER TABLE `orders` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Temporary view structure for view `ordersview`
+--
+
+DROP TABLE IF EXISTS `ordersview`;
+/*!50001 DROP VIEW IF EXISTS `ordersview`*/;
+SET @saved_cs_client     = @@character_set_client;
+/*!50503 SET character_set_client = utf8mb4 */;
+/*!50001 CREATE VIEW `ordersview` AS SELECT 
+ 1 AS `OrderID`,
+ 1 AS `Quantity`,
+ 1 AS `TotalCost`*/;
+SET character_set_client = @saved_cs_client;
 
 --
 -- Table structure for table `staff`
@@ -228,7 +248,7 @@ CREATE TABLE `staff` (
   `Salary` decimal(10,0) NOT NULL,
   `ContactNumber` varchar(55) NOT NULL,
   PRIMARY KEY (`StaffID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -237,8 +257,27 @@ CREATE TABLE `staff` (
 
 LOCK TABLES `staff` WRITE;
 /*!40000 ALTER TABLE `staff` DISABLE KEYS */;
+INSERT INTO `staff` VALUES (1,'Jammy Jamerson','Manager',10000,'55545687'),(2,'Shawn smith','waiter',50000,'65489753'),(3,'Henry Heart','chef',75000,'465465478');
 /*!40000 ALTER TABLE `staff` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Final view structure for view `ordersview`
+--
+
+/*!50001 DROP VIEW IF EXISTS `ordersview`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = utf8mb4 */;
+/*!50001 SET character_set_results     = utf8mb4 */;
+/*!50001 SET collation_connection      = utf8mb4_0900_ai_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`littleLemonUser`@`%` SQL SECURITY DEFINER */
+/*!50001 VIEW `ordersview` AS select `orders`.`OrderID` AS `OrderID`,`orders`.`Quantity` AS `Quantity`,`orders`.`TotalCost` AS `TotalCost` from `orders` where (`orders`.`Quantity` > 2) */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -249,4 +288,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-08-26  9:46:28
+-- Dump completed on 2024-08-26 13:19:17
